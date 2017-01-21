9b53dba326cdd636fc2194065326be3e5d25d924
Dave Brosius <dbrosius@mebigfatguy.com>
2017-01-21 16:53:10 -0500

    update cp dates

--- src/main/java/com/mebigfatguy/collatz4j/CollatzDisplay.java
+++ src/main/java/com/mebigfatguy/collatz4j/CollatzDisplay.java
@@ -1,7 +1,7 @@
 /*
  * collatz4j - a visualization of the Collatz Conjecture
- * Copyright 2016 MeBigFatGuy.com
- * Copyright 2016 Dave Brosius
+ * Copyright 2016-2017 MeBigFatGuy.com
+ * Copyright 2016-2017 Dave Brosius
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
